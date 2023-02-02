9e896132f5eb9d63f0b5c6711e87ff65c00fa93b
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-02-02 11:12:26 -0800

    open and close accordion on active

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,14 +1,24 @@
 import React, { useState } from "react";
+import { GoChevronLeft, GoChevronDown } from "react-icons/go";
 function Accordion({ items }) {
-  const [expandedIndex, setExpandedIndex] = useState(0);
+  const [expandedIndex, setExpandedIndex] = useState(1);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  }
 
   const renderedItems = items.map((item, index) => {
     const isExpanded = index === expandedIndex;
-
+    const icon = <span>
+      {isExpanded ? <GoChevronDown /> : <GoChevronLeft />}
+    </span>
     return (
       <div key={item.id}>
-        <div>{item.label}</div>
-        <div>{item.content}</div>
+        <div onClick={() => handleClick(index)} >
+          {icon}
+          {item.label}
+        </div>
+        {isExpanded && <div>{item.content}</div>}
       </div>
     );
   });

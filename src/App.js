deea09238322d1c056207a2fec93155bdc6c9112
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-02-07 09:32:12 -0800

    move everytinig over to DropdownPage git push

--- src/App.js
+++ src/App.js
@@ -1,23 +1,5 @@
-import { useState } from "react";
-import Dropdown from "./components/Dropdown";
 function App() {
-  const [selection, setSelection] = useState(null);
-  const handleSelect = (option) => {
-    setSelection(option);
-    console.log(selection);
-  };
-  const options = [
-    { label: "Red", value: "red" },
-    { label: "Green", value: "green" },
-    { label: "Blue", value: "blue" },
-  ];
-
-  return (
-    <div className="flex">
-      <Dropdown value={selection} options={options} onChange={handleSelect} />
-      <Dropdown value={selection} options={options} onChange={handleSelect} />
-    </div>
-  );
+  return <div>app</div>;
 }
 
 export default App;

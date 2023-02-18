a15b702a107cb0d9ccc0040981abd2d881eaff1b
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-02-18 11:09:31 -0800

    some styling

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,17 +1,26 @@
+import classNames from "classnames";
 import { useContext } from "react";
 import NavigationContext from "../context/navigation";
 
 function Link({ to, children }) {
   const { navigate } = useContext(NavigationContext);
-  console.log(navigate);
+
+  const classes = classNames("text-blue-500");
 
   const handleClick = (e) => {
+    if (e.metaKey || e.ctrlKey) {
+      return;
+    }
     e.preventDefault();
 
     navigate(to);
   };
 
-  return <a onClick={handleClick}>{children}</a>;
+  return (
+    <a className={classes} href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;

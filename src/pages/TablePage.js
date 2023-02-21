b5991c6c31da35e5264ee752ef8847aad071017f
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-02-21 12:38:35 -0800

    innit table comp

---
+++ src/pages/TablePage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Table from "../components/Table";
+function TablePage() {
+  return (
+    <div>
+      <Table></Table>
+    </div>
+  );
+}
+
+export default TablePage;

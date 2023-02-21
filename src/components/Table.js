8dc8e54bc5702749d065774e58fdf520fc409dad
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-02-21 13:00:40 -0800

    rendered table with data

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,7 +1,31 @@
+import { render } from "@testing-library/react";
 import React from "react";
 
-function Table() {
-  return <div>table</div>;
+function Table({ data }) {
+  const renderedRows = data.map((fruit) => {
+    return (
+      <tr className="border-b" key={fruit.name}>
+        <td className="p-3">{fruit.name}</td>
+        <td>
+          <div className={`p-3 m-2 ${fruit.color}`}></div>
+        </td>
+        <td>{fruit.score}</td>
+      </tr>
+    );
+  });
+
+  return (
+    <table className="table-auto border-spacing-2">
+      <thead>
+        <tr className="border-b-2">
+          <th>Fruit</th>
+          <th>Color</th>
+          <th>Score</th>
+        </tr>
+      </thead>
+      <tbody>{renderedRows}</tbody>
+    </table>
+  );
 }
 
 export default Table;
--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Table from "../components/Table";
 function TablePage() {
+  const data = [
+    { name: "Orange", color: "bg-orange-500", score: 5 },
+    { name: "Apple", color: "bg-red-500", score: 3 },
+    { name: "Banana", color: "bg-yellow-300", score: 1 },
+    { name: "Lime", color: "bg-green-400", score: 4 },
+  ];
   return (
     <div>
-      <Table></Table>
+      <Table data={data}></Table>
     </div>
   );
 }

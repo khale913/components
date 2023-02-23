667d4143eaeebf6149c78e384d76349c0ba8ec3e
Kelton Hale <77764287+khale913@users.noreply.github.com>
2023-02-23 09:18:24 -0800

    adding Fragment to table header

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,10 +1,9 @@
-import { render } from "@testing-library/react";
-import React from "react";
+import { Fragment } from "react";
 
 function Table({ data, config, keyFn }) {
   const renderedHeaders = config.map((column) => {
     if (column.header) {
-      return column.header();
+      return <Fragment key={column.label}>{column.header()}</Fragment>;
     }
     return <th key={column.label}>{column.label}</th>;
   });
